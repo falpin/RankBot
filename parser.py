990e9ff7dab77c1da0c247142a90fc61ecd12d8e
falbue <cyansair05@gmail.com>
2025-07-18 20:40:51 +0500

    Настройка драйвера в отдельной функции

--- parser.py
+++ parser.py
@@ -38,13 +38,8 @@ def load_data():
     except (FileNotFoundError, json.JSONDecodeError, KeyError):
         return None
 
-def scrape_magtu_data():
-    if is_cache_valid():
-        cached_data = load_data()
-        if cached_data:
-            print("Используются кэшированные данные")
-            # return cached_data
 
+def setup_driver():  # настройки драйвера
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
@@ -70,67 +65,76 @@ def scrape_magtu_data():
         driver = webdriver.Chrome(service=service, options=options)
         wait = WebDriverWait(driver, 1)  # Увеличиваем время ожидания до 1 секунд
         
-        result_dict = {}
+        return driver, wait
         
-        try:
-            print("Открываем страницу")
-            driver.get("https://www.magtu.ru/abit/6013-spiski-podavshikh-dokumenty-byudzhetnye-mesta.html")
-            
-            # Ждем и выбираем институт
-            institute_select = wait.until(EC.presence_of_element_located((By.ID, "dep")))
-            Select(institute_select).select_by_value("08")
-            
-            # Ждем и выбираем специальность
-            spec_select = wait.until(EC.presence_of_element_located((By.ID, "spec")))
-            found_specialty = False
-            for option in Select(spec_select).options:
-                if "Web-приложений" in option.text:
-                    option.click()
-                    found_specialty = True
-                    break
-            
-            if not found_specialty:
-                print("Специальность 'Web-приложений' не найдена")
-                return {}
-            
-            # Даем дополнительное время для загрузки таблицы
-            time.sleep(0.3)
-            
-            # Парсим таблицу
-            table = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "table")))
-            rows = table.find_elements(By.TAG_NAME, "tr")[1:]  # Пропускаем заголовок
-            
-            for row in rows:
-                cells = row.find_elements(By.TAG_NAME, "td")
-                if len(cells) < 8:
-                    continue
-                    
-                snils_id = cells[0].text.strip()
-                
-                # Проверяем наличие класса 'yes' в строке
-                admitted = "yes" in row.get_attribute("class").split()
-                
-                result_dict[snils_id] = {
-                    "Баллы": cells[1].text.strip(),
-                    "Доп": cells[2].text.strip(),
-                    "Оригинал": cells[4].text.strip(),
-                    "Основание приема": cells[5].text.strip(),
-                    "Приоритет": int(cells[7].text.strip()),
-                    "Поступил": admitted,
-                    "Экзамены": cells[3].text.strip()
-                }
-            save_data(result_dict)
-            return result_dict
-            
-        except Exception as e:
-            print(f"Произошла ошибка при работе с сайтом: {str(e)}")
-            return {}
-        finally:
-            driver.quit()
-            
     except Exception as e:
         print(f"Произошла ошибка при настройке драйвера: {str(e)}")
+        return None, None
+
+
+def scrape_magtu_data(speciality):
+    if is_cache_valid():
+        cached_data = load_data()
+        if cached_data:
+            print("Используются кэшированные данные")
+            return cached_data
+
+    driver, wait = setup_driver()
+    if not driver or not wait:
         return {}
+    
+    result_dict = {}
+    
+    try:
+        print("Открываем страницу")
+        driver.get("https://www.magtu.ru/abit/6013-spiski-podavshikh-dokumenty-byudzhetnye-mesta.html")
+        
+        institute_select = wait.until(EC.presence_of_element_located((By.ID, "dep")))
+        Select(institute_select).select_by_value("16")
+        
+        spec_select = wait.until(EC.presence_of_element_located((By.ID, "spec")))
+        found_specialty = False
+        for option in Select(spec_select).options:
+            if speciality in option.text:
+                option.click()
+                found_specialty = True
+                break
+        
+        if not found_specialty:
+            print(f"Специальность '{speciality}' не найдена")
+            return {}
+        
+        time.sleep(0.3)  # дополнительное время для загрузки таблицы
+        
+        table = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "table")))
+        rows = table.find_elements(By.TAG_NAME, "tr")[1:]  # Пропускаем заголовок
+        
+        for row in rows:
+            cells = row.find_elements(By.TAG_NAME, "td")
+            if len(cells) < 8:
+                continue
+                
+            snils_id = cells[0].text.strip()
+            
+            admitted = "yes" in row.get_attribute("class").split()
+            
+            result_dict[snils_id] = {
+                "Баллы": cells[1].text.strip(),
+                "Доп": cells[2].text.strip(),
+                "Оригинал": cells[4].text.strip(),
+                "Основание приема": cells[5].text.strip(),
+                "Приоритет": int(cells[7].text.strip()),
+                "Поступил": admitted,
+                "Экзамены": cells[3].text.strip()
+            }
+        save_data(result_dict)
+        return result_dict
+        
+    except Exception as e:
+        print(f"Произошла ошибка при работе с сайтом: {str(e)}")
+        return {}
+    finally:
+        driver.quit()
 
 
 def get_applicant_priorities(snils):
